875525940046885617f79f1fab5e18a92ae7c637
Karla R <karla.y.reyes12@gmail.com>
2018-07-09 19:08:31 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,10 @@
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
   loop do
     puts phrase
+  break
+    if counter > 7
   end
 end
 
